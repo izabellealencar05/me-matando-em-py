22f4c96fafffea5304fadfffc7785613f378e07d
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-08-21 18:43:37 -0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,7 @@
+#Fazer um programa para ler dois valores inteiros X e Y, e depois mostrar na tela o valor da soma destes
+#números. 
+a = int(input("digite um valor: "))
+b = int(input("digite um outro valor: "))
+soma = a + b
+print(f"a soma entre esses valores é {soma}")
+
